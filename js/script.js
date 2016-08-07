2a1eaa9fccb86335b1e71678e3615097bda0c935
Ростислав <extra0@mail.ru>
2016-08-07 21:29:08 +0300

    главная 80%

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ $(function(){
 	// вызов фенсибокса
 	$('[fancybox]').fancybox();
 
+	// только цифры в инпутах
+	$('[only-numbers]').bind("change keyup input click", function() {
+		if (this.value.match(/[^0-9\.\+]/g, '')) {
+			this.value = this.value.replace(/[^0-9]/g, '');
+		}
+	});
+
+	// слайдер акций
+	$('.action-slider__list').bxSlider({
+		mode: 'fade'
+	});
+
 });
