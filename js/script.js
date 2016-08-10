20a1a200d12e1f7a775e85b554bcad4eca869659
Ростислав <extra0@mail.ru>
2016-08-10 14:00:35 +0300

    полный адаптив главной

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 $(function(){
 
 	// вызов фенсибокса
-	$('[fancybox]').fancybox();
+	$('[fancybox]').fancybox({
+		closeBtn: false
+	});
+
+	$('.modal__close').on('click', function(){
+		$.fancybox.close();
+	});
 
 	// только цифры в инпутах
 	$('[only-numbers]').bind("change keyup input click", function() {
@@ -10,9 +16,63 @@ $(function(){
 		}
 	});
 
+	// collapse
+	$('[collapse]').on('click',function(){
+		$('[collapse]').toggleClass('active');
+		$('.header__menu').toggleClass('active');
+	});
+
 	// слайдер акций
 	$('.action-slider__list').bxSlider({
 		mode: 'fade'
+	});
+
+	// слайдер отелей
+	var hSlider = $('.hotels-choose__slider').bxSlider({
+		pager: false,
+		infiniteLoop: false,
+		adaptiveHeight: true,
+		hideControlOnEnd: true,
+		onSlideBefore: function(){ // меняем состояние точек при смене слайдов
+			$('.hotels-choose__map-dot').removeClass('active');
+	    	$('.hotels-choose__map-dot[data-index='+ hSlider.getCurrentSlide() +']').addClass('active');
+	  	},
+	  	onSliderLoad: function() {
+	  		$('.hotels-choose__slider-item').each(function(k){
+	  			$('.hotels-choose__map-dot').eq(k).attr('data-category', $(this).attr('data-category')); // проставляем категории для точек
+	  		});
+	  	}
+	});
+
+	// меняем активные состояния кнопок фильтра
+	$('.hotels-choose__filter-list-trigger').on('click', function(){
+		$('.hotels-choose__filter-list-trigger').removeClass('active');
+		$(this).addClass('active');
+
+		$('.hotels-choose__map-dot[data-category='+ $(this).attr('data-category') +']').hide();
+	});
+
+	$('.hotels-choose__map-dot').each(function(i){$(this).attr('data-index', i);}); // проставляем индексы на точки
+	$('.hotels-choose__map-dot[data-index='+ hSlider.getCurrentSlide() +']').addClass('active'); // активируем первую точку
+
+	// меняем слайды по клику на точки
+	$('.hotels-choose__map-dot').on('click', function(){
+		$('.hotels-choose__map-dot').removeClass('active');
+		$(this).addClass('active');
+		hSlider.goToSlide(parseInt($(this).attr('data-index')));
+	});
+
+
+	// открываем меню
+	var menu = $('.header__menu'),
+		collapse = $('[collapse]');
+
+	// закрываем меню по клику вне области
+	$(document).mouseup(function(e) {
+		if (collapse.has(e.target).length === 0 && menu.has(e.target).length === 0) {
+			menu.removeClass('active');
+			collapse.removeClass('active');
+		}
 	});
 
 });
